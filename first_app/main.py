8dcf87b1106ac7946ec4784bb12224f8763fc2e6
Andriy Rukas <rukasav@gmail.com>
2024-04-12 11:30:55 +0300

    feat: add starter code for the first app

---
+++ first_app/main.py
@@ -0,0 +1,21 @@
+# Import the modules
+from PyQt5 import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+
+# Main app Objects and Settings
+app = QApplication([])
+main_window = QWidget()
+main_window.setWindowTitle("Random Word Maker")
+main_window.resize(300, 200)
+
+
+# Create all App Objects
+
+# All design here
+
+# Events
+
+# Show/Run the app
+
+main_window.show()
+app.exec_()
