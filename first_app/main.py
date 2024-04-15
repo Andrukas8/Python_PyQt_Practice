1a63891b4be495e7c9b26fdd155bb00b1c200dc7
Andriy Rukas <rukasav@gmail.com>
2024-04-15 11:08:57 +0300

    feat: add functionality

--- first_app/main.py
+++ first_app/main.py
@@ -2,6 +2,8 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
 
+from random import choice
+
 # Main app Objects and Settings
 app = QApplication([])
 main_window = QWidget()
@@ -18,13 +20,15 @@ button1 = QPushButton("Click Me")
 button2 = QPushButton("Click Me")
 button3 = QPushButton("Click Me")
 
+my_words = ['hello', 'goodbye', 'test', 'app',
+            'python', 'pyqt', 'vietnam', 'asia']
+
 # All design here
 master_layout = QVBoxLayout()
 row1 = QHBoxLayout()
 row2 = QHBoxLayout()
 row3 = QHBoxLayout()
 
-#
 row1.addWidget(title, alignment=Qt.AlignCenter)
 
 row2.addWidget(text1, alignment=Qt.AlignCenter)
@@ -35,16 +39,35 @@ row3.addWidget(button1)
 row3.addWidget(button2)
 row3.addWidget(button3)
 
-
 master_layout.addLayout(row1)
 master_layout.addLayout(row2)
 master_layout.addLayout(row3)
 
 main_window.setLayout(master_layout)
 
+# Create Functions
+
+
+def random_word1():
+    word = choice(my_words)
+    text1.setText(word)
+
+
+def random_word2():
+    word = choice(my_words)
+    text2.setText(word)
+
+
+def random_word3():
+    word = choice(my_words)
+    text3.setText(word)
+
+
 # Events
+button1.clicked.connect(random_word1)
+button2.clicked.connect(random_word2)
+button3.clicked.connect(random_word3)
 
 # Show/Run the app
-
 main_window.show()
 app.exec_()
